ad7dea1faae8bd98c74a7f6305c035419e1a71bb
biboy7777 <biboyilano777@gmail.com>
2024-03-18 21:31:32 +0800

    added sample Login Page

--- UI/src/App.jsx
+++ UI/src/App.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import Login from "./pages/Login"
+// import TypeofUser from "./pages/TypeofUser";
 
 const App = () => {
   return (
-    <div className="text-red-500">
-      <p>Hello World</p>
+    <div className="container mx-auto">
+      <>
+      <Login />
+      </>
     </div>
+    // <>
+    // <div className="text-red-500">
+    //   <p>Hello World</p>
+    // </div>
+    // <Login/>
+    // </>
+
   )
 };
 
