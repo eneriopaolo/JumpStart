88beefea4c61a285a13aaa6e399102c8f84fc3b7
biboy7777 <biboyilano777@gmail.com>
2024-03-18 21:32:57 +0800

    added spacing in the code

--- UI/src/pages/Login.jsx
+++ UI/src/pages/Login.jsx
@@ -3,11 +3,15 @@ import React from "react";
 const Login = () => {
     return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-500">
+
       <div className="w-full max-w-md px-8 py-12 bg-white rounded-lg shadow-md">
+
         <div className="text-center mb-8">
           <h1 className="text-3xl font-bold text-gray-800">Log in to HireIndex</h1>
         </div>
+        
         <form className="space-y-4">
+
           <div className="flex flex-col space-y-2">
             <label htmlFor="usernameOrEmail" className="text-sm font-medium text-gray-700">
               Username or Email
@@ -19,6 +23,7 @@ const Login = () => {
               placeholder="Enter your username or email"
             />
           </div>
+
           <div className="flex flex-col space-y-2">
             <label htmlFor="password" className="text-sm font-medium text-gray-700">
               Password
@@ -30,17 +35,20 @@ const Login = () => {
               placeholder="Enter your password"
             />
           </div>
+
           <button
             type="submit"
             className="w-full py-2 px-4 text-center bg-red-700 text-white rounded-lg hover:bg-red-900"
           >
             Continue
           </button>
+
           <div className="text-center mt-4">
             <a href="#" className="text-sm text-red-700 hover:underline">
               Forgot Password?
             </a>
           </div>
+
           <div className="text-center mt-12">
             <p className="text-sm text-gray-500">
               Don't have an account? 
