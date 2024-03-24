852f2210931c7337b8a1e3b2c7323a364ff7477a
eneriopaolo <paoloenerio@gmail.com>
2024-03-24 16:16:33 +0800

    Extracted Search Bar from Nav Bar

---
+++ UI/src/components/JobSearchBar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const JobSearchBar = () => {
+    return (
+        <div className="flex justify-center">
+            <input
+                type="text"
+                placeholder="Search Jobs"
+                className="w-full px-4 py-2 mr-2 text-gray-800 placeholder-gray-500 bg-gray-200 border border-gray-300 rounded-lg focus:outline-none focus:bg-white focus:border-blue-500"
+            />
+            <button
+                className="px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none">
+                Search
+            </button>
+        </div>
+    );
+};
+
+export default JobSearchBar;
--- UI/src/components/JobSeekerNavBar.jsx
+++ UI/src/components/JobSeekerNavBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import JobSearchBar from "./JobSearchBar";
 
 const JobSeekerNavBar = () => {
     const userData = JSON.parse(localStorage.getItem('userData'));
@@ -16,18 +17,7 @@ const JobSeekerNavBar = () => {
                 <p className="p-4 cursor-pointer text-gray-800 hover:text-red-900 hover:bg-blue-500"><Link to="/" className="">Logo</Link></p>
                 <p className="p-4 cursor-pointer text-gray-800 hover:text-red-900 hover:bg-blue-500"><Link to="/" className="text-gray-800 hover:text-gray-900">Find Jobs</Link></p>
             </div>
-            <div className="flex justify-center">
-                <input
-                    type="text"
-                    placeholder="Search Jobs"
-                    className="w-full px-4 py-2 mr-2 text-gray-800 placeholder-gray-500 bg-gray-200 border border-gray-300 rounded-lg focus:outline-none focus:bg-white focus:border-blue-500"
-                />
-                <button
-                    className="px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none"
-                >
-                    Search
-                </button>
-            </div>
+            <JobSearchBar/>
             <div>
                 <span className="text-gray-800">{userName}</span>
             </div>
