080f6f341309709794a302f92c06ef13650c3945
Patrick-Borj-Tuano <2022pbntuao@live.mcl.edu.ph>
2024-03-27 02:37:19 +0800

    MOVED COMPONENTS FILES

--- UI/src/App.jsx
+++ UI/src/App.jsx
@@ -4,13 +4,13 @@ import TypeofUser from "./pages/TypeofUser";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUpEmployer from "./pages/SignUpEmployer";
 import SignUpJobSeeker from "./pages/SignUpJobSeeker";
-import JobSeekerHomePage from "./pages/JobSeekerHomePage"; 
-import JobSeekerProfilePage from "./pages/JobSeekerProfilePage"; 
+import JobSeekerHomePage from "./pages/JobSeekerHomePage";
+import JobSeekerProfilePage from "./pages/JobSeekerProfilePage";
 import EmployerHomePage from "./pages/EmployerHomePage";
 import PostJobPage from "./pages/PostJobPage";
-import FindJobHomePage from "./pages/FindJobHomePage"
-import JobProfile from "./components/JobProfile"
-import ApplicationStatusPage from "./pages/ApplicationStatusPage"
+import FindJobHomePage from "./pages/FindJobHomePage";
+import JobProfile from "./components/JobProfile";
+import ApplicationStatusPage from "./pages/ApplicationStatusPage";
 
 const App = () => {
   return (
@@ -22,12 +22,12 @@ const App = () => {
           <Route path="/signup-employer" element={<SignUpEmployer />} />
           <Route path="/signup-jobseeker" element={<SignUpJobSeeker />} />
           <Route path="/jobseeker-home-page" element={<JobSeekerHomePage />} />
-          <Route path="/jobseeker-profile-page" element={<JobSeekerProfilePage />} />
-          <Route path="/employer-home-page" element={<EmployerHomePage/>}/>
-          <Route path="/find-job-page" element={<FindJobHomePage/>}/>
-          <Route path="/view-profile-page" element={<JobProfile/>}/>
-          <Route path="/post-job-page" element={<PostJobPage/>}/>
-          <Route path="/view-application-status" element={<ApplicationStatusPage/>}/>
+          <Route path="/jobseeker-profile-page"element={<JobSeekerProfilePage />}/>
+          <Route path="/employer-home-page" element={<EmployerHomePage />} />
+          <Route path="/find-job-page" element={<FindJobHomePage />} />
+          <Route path="/view-profile-page" element={<JobProfile />} />
+          <Route path="/post-job-page" element={<PostJobPage />} />
+          <Route path="/view-application-status" element={<ApplicationStatusPage />}/>
         </Routes>
       </div>
     </BrowserRouter>
