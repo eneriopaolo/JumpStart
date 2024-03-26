7dc3542f0abb36f6af42a59d09d6c565089d899e
Patrick-Borj-Tuano <2022pbntuao@live.mcl.edu.ph>
2024-03-27 02:32:51 +0800

    MOVED THE COMPONENTS

--- UI/src/pages/EmployerHomePage.jsx
+++ UI/src/pages/EmployerHomePage.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import EmployerNavBar from "./EmployerNavBar";
-import EmployerContent from "./EmployerContent"
+import EmployerNavBar from "../components/EmployerNavBar";
+import EmployerContent from "../components/EmployerContent";
 
 const JobSeekerHomePage = () => {
-    return (
-        <div>
-            <EmployerNavBar />
-            <EmployerContent/>
-        </div>
-    );
+  return (
+    <div>
+      <EmployerNavBar />
+      <EmployerContent />
+    </div>
+  );
 };
 
 export default JobSeekerHomePage;
--- UI/src/pages/FindJobHomePage.jsx
+++ UI/src/pages/FindJobHomePage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import JobSeekerNavBar from "../components/JobSeekerNavBar";
-import FindJobContent from "../pages/FindJobContent";
+import FindJobContent from "../components/FindJobContent";
 
 const JobSeekerHomePage = () => {
     return (
