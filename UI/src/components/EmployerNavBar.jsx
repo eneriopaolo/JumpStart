ab158d54c800090572beae78e878d9a0e8829d50
eneriopaolo <paoloenerio@gmail.com>
2024-03-27 15:22:00 +0800

    Removed Search Bar from EmpNavBar

--- UI/src/components/EmployerNavBar.jsx
+++ UI/src/components/EmployerNavBar.jsx
@@ -36,16 +36,6 @@ const EmployerNavBar = () => {
           </Link>
         </p>
       </div>
-      <div className="flex justify-center">
-        <input
-          type="text"
-          placeholder="Search Jobs"
-          className="w-full px-4 py-2 mr-2 text-gray-800 placeholder-gray-500 bg-gray-200 border border-gray-300 rounded-lg focus:outline-none focus:bg-white focus:border-blue-500"
-        />
-        <button className="px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none">
-          Search
-        </button>
-      </div>
       <div>
           <button className="text-gray-800 flex items-center justify-center min-w-20" onClick={toggleMenu}>
               {userName} <span className="ml-2"></span>
