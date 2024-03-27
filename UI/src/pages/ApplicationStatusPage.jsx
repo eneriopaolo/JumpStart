f937d34395ec418ecaae8074c35e03a651ea85a6
eneriopaolo <paoloenerio@gmail.com>
2024-03-27 19:00:11 +0800

    Application Status Edit

--- UI/src/pages/ApplicationStatusPage.jsx
+++ UI/src/pages/ApplicationStatusPage.jsx
@@ -66,9 +66,9 @@ const ApplicationStatusPage = () => {
 
   return (
     <div className="application-status-page bg-gray-200 p-4 relative min-h-screen">
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between h-24">
         <h1 className="text-2xl font-bold mb-4">Application Status</h1>
-        <button><Link to="/jobseeker-home-page">Back</Link></button>
+        <button className="px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none"><Link to="/jobseeker-home-page">Back</Link></button>
       </div>
       {isLoading ? (
         <p>Loading...</p>
