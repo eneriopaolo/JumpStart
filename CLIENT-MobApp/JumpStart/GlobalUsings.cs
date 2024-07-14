0414214dd8faa925090818448898be16ba2c6db4
eneriopaolo <paoloenerio@gmail.com>
2024-07-14 20:18:54 +0800

    Installed NuGet Packages

--- CLIENT-MobApp/JumpStart/GlobalUsings.cs
+++ CLIENT-MobApp/JumpStart/GlobalUsings.cs
@@ -1,9 +1,12 @@
 ﻿// Built-In Namespaces & References:
-global using System.Text.Json;
 global using System.Diagnostics;
 global using System.Runtime.CompilerServices;
 global using System.ComponentModel;
 global using System.Collections.ObjectModel;
+
+// NuGet Packages
+global using System.Text.Json;
+global using CommunityToolkit.Mvvm;
 
 // Project Specific Files & Folders/Directories:
 global using JumpStart.Model;
