49663231b0f8a0d46410cdce7c20f9fa395bd4fa
Chris <ckvillanueva07@gmail.com>
2024-07-19 19:33:47 +0800

    fixed "unable to register due to typeofuser"

--- CLIENT-MobApp/JumpStart/View/RegisterPage.xaml.cs
+++ CLIENT-MobApp/JumpStart/View/RegisterPage.xaml.cs
@@ -41,8 +41,9 @@ namespace JumpStart
                 email = email,
                 name = name,
                 password = password,
-                
- 
+                typeofuser = userType
+
+
             };
 
             try
